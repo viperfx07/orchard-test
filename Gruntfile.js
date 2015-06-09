6c1d44d0e66b2c11e6855d8f39286893220e37df
unknown <victor.tanudjaja@LAB311-STAFF.mqc-staff.wynyardgreen.local>
2015-06-09 20:42:31 +1000

    initial

---
+++ Gruntfile.js
@@ -0,0 +1,126 @@
+module.exports = function(grunt) {
+
+    // 0. Speed up the grunt
+    require('jit-grunt')(grunt);
+
+    // 1. All configuration goes here
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+
+        concat: {
+            main: {
+                src: ['src/js/main.js'],
+                dest: 'build/assets/js/global.js'
+            },
+        },
+
+        uglify: {
+            my_target: {
+                files: {
+                    'build/assets/js/global.min.js': ['build/assets/js/global.js']
+                }
+            }
+        },
+
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/img/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'build/assets/img/'
+                }],
+                options: {
+                    cache: false
+                }
+            }
+        },
+
+        concurrent: {
+            first: {
+              tasks: ['css', 'js', 'jade']
+            }
+        },
+
+        watch: {
+            options: {
+                livereload: true,
+            },
+            scripts: {
+                files: ['src/js/*.js'],
+                tasks: ['concat', 'uglify', 'notify:js'],
+                options: {
+                    spawn: false,
+                },
+            },
+            css: {
+                files: ['src/**/*.scss'],
+                tasks: ['sass:dist', 'notify:css'],
+                options: {
+                    spawn: false,
+                }
+            },
+            html: {
+                options: {
+                    livereload: true
+                },
+                files: ['**/*.jade']
+            },
+        },
+
+        jade: {
+            compile: {
+                options: {
+                	pretty: true,
+                    data: {
+                        debug: false
+                    }
+                },
+                files: {
+                    "build/index.html": ["src/jade/index.jade"]
+                }
+            }
+        },
+
+        sass: {
+            dist: {
+                options: {
+                    style: 'compact',
+                    compass: true
+                },
+                files: {
+                    'build/assets/css/main.css': 'src/scss/main.scss'
+                }
+            }
+        },
+
+        notify: {
+            css: {
+                options: {
+                    enabled: true,
+                    title: 'Grunt Complete', // optional
+                    message: '[sass:dist] finished', //required
+                }
+            },
+            js: {
+                options: {
+                    enabled: true,
+                    title: 'Grunt Complete', // optional
+                    message: '[scripts] finished', //required
+                }
+            }
+        },
+
+    });
+
+    // 3. Where we tell Grunt we plan to use this plug-in.
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+    grunt.registerTask('css', ['sass:dist']);
+    grunt.registerTask('js', ['concat', 'uglify']);
+    grunt.registerTask('img', ['imagemin']);
+    grunt.registerTask('default', ['concat', 'uglify', 'imagemin', 'jade', 'sass:dist', 'watch']);
+    grunt.registerTask('me', 'concurrent:first');
+};
---
+++ readme.txt
@@ -0,0 +1,19 @@
+Test Instructions
+--------------------------------------------------------------------
+
+Please supply a zip file containing the html and necessary files needed to build the webpage in the attached jpeg.
+
+You are free to build the page as you think appropriate (front end frameworks, JS libraries, CSS Preprocessor etc.). Please also ensure you implement the tree structure on the left hand side using JavaScript.
+
+Think of this page as part of a much larger project, don’t treat this as just one page, and think about how components can be easily re-used in other pages of this site. 
+In Orchard we support: IE8+, latest Chrome, Firefox and Safari. 
+ 
+
+It's your chance to stand out, show off your approach to building a great interface, and maybe teach us something along the way.
+
+
+Thanks,
+
+
+Orchard
+Technology Team
